9fd1f9f757a90716021b57ea877110968cab3117
k1rh4 <k1rh4.lee@gmail.com>
2018-03-21 22:51:07 +0900

    Make build sysem via clang v1.1

--- src/socket.h
+++ src/socket.h
@@ -6,8 +6,12 @@ class Socket
 {
 public:
     Socket();
+<<<<<<< HEAD
     int SendSocket();
     int RecvSocket();
+=======
+
+>>>>>>> 14e1a65ace3209876351972c9a2e2038f539385e
     // Initialize
     // SendSocket
     // RecvSocket
--- src/user_info.h
+++ src/user_info.h
@@ -6,8 +6,12 @@ class UserInfo
 {
 public:
     UserInfo();
+<<<<<<< HEAD
     ~UserInfo();
     //virtual ~UserInfo();
+=======
+    virtual ~UserInfo();
+>>>>>>> 14e1a65ace3209876351972c9a2e2038f539385e
 
     // GetGithubMail
     // UpdateUserInfo
