33409d3f61a3c81615abb83ef6bdaa7db59b079c
Sunnyeo Park <psnyeo88@kaist.ac.kr>
2018-03-21 16:32:41 +0900

    Add UserInfo Class file

---
+++ src/user_info.h
@@ -0,0 +1,21 @@
+#ifndef USER_INFO_H
+#define USER_INFO_H
+
+
+class UserInfo
+{
+public:
+    UserInfo();
+    virtual ~UserInfo();
+
+    // GetGithubMail
+    // UpdateUserInfo
+    // Getter, Setter
+
+private:
+    // GithubID
+    // IPAddress
+    // PGPKey
+};
+
+#endif // USER_INFO_H
