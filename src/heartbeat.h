7bf962101d28d14598cfd50e839632d5f9f42edb
Sunnyeo Park <psnyeo88@kaist.ac.kr>
2018-03-21 16:31:24 +0900

    Add Heartbeat class file

---
+++ src/heartbeat.h
@@ -0,0 +1,16 @@
+#ifndef HEARTBEAT_H
+#define HEARTBEAT_H
+
+
+class Heartbeat
+{
+public:
+    Heartbeat();
+    virtual ~Heartbeat();
+
+    // Initialize
+    // SendBroadcast
+    // RecvBroadcast
+};
+
+#endif // HEARTBEAT_H
