ec371578180ea915d6b1fd7ab9bae877e2d181f4
Sunnyeo Park <psnyeo88@kaist.ac.kr>
2018-03-23 10:10:59 +0900

    Add class constructor

--- src/heartbeat.cpp
+++ src/heartbeat.cpp
@@ -1,16 +1,39 @@
-#include "heartbeat.h"
+nclude "heartbeat.h"
 
 Heartbeat::Heartbeat()
 {
-
+    Initialize();
 }
 
-int Heartbeat::RecvBroadcast()
+int Heartbeat::Initialize()
+{
+    CreateSocket();
+    SetSocket();
+    return 0;
+}
+
+int Heartbeat::CreateSocket()
+{
+}
+
+int Heartbeat::SetSocket()
+{
+}
+
+void Heartbeat::Start()
+{
+}
+
+void Heartbeat::SendBroadcast()
+{
+}
+
+void Heartbeat::RecvBroadcast()
 {
     // Thread
     // if new user info
     // userinfo.getmail
     // pgp.searchkeys
     // updateUserinfo
-    return 0;
 }
+
