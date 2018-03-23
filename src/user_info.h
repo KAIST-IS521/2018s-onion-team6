b752f73abbb8700b794807ef22dec56370f23c57
Sunnyeo Park <psnyeo88@kaist.ac.kr>
2018-03-23 12:44:41 +0900

    Add user info fields and getter, setter

--- src/user_info.h
+++ src/user_info.h
@@ -1,22 +1,25 @@
 #ifndef USER_INFO_H
 #define USER_INFO_H
 
-
 class UserInfo
 {
 public:
     UserInfo();
     ~UserInfo();
-    //virtual ~UserInfo();
 
-    // GetGithubMail
-    // UpdateUserInfo
-    // Getter, Setter
+    string GetGithubId();
+    void SetGithubId(string github_id);
+
+    string GetIpAddr();
+    void SetIpAddr(string ip_Addr);
+
+    string GetPGPKeyId();
+    void SetPGPKeyId(string pgp_key_id);
 
 private:
-    // GithubID
-    // IPAddress
-    // PGPKey
+    string github_id;
+    string ip_addr;
+    string pgp_key_id;
 };
 
 #endif // USER_INFO_H
