d1ff07fb1373e9665a9d4e15d680ed564488b2d5
soohun Kim <soohunkim@kaist.ac.kr>
2018-03-26 12:38:07 +0900

    Add PgpManager

---
+++ src/test/pgpmanager/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "PgpManager.h"
+
+using namespace std;
+
+int main(){
+
+    cout << "Test Start !" << endl;
+    PgpManager *mPgpManager = new PgpManager();
+
+    return 0;
+}
