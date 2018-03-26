1697c6cb2e215f56930ad3105b5bf71fbb649a23
Sunnyeo Park <psnyeo88@kaist.ac.kr>
2018-03-26 12:58:37 +0900

    Change get password and function name

--- src/shell.h
+++ src/shell.h
@@ -3,6 +3,9 @@
 
 #include<iostream>
 #include<list>
+#include<cstring>
+#include <cstdio>
+#include <termios.h>
 
 #include "user_info.h"
 #include "msgserver.h"
@@ -15,27 +18,37 @@ extern unordered_map<string, UserInfo*> UserInfoMap;
 
 class Shell
 {
+    public:
+        Shell();
+        ~Shell();
+        void Run();
+
     private:
         string private_id;
         string private_pw;
         list <string> prompt;
         string receiver;
+        MsgClient *msg_client;
 
-    public:
-        Shell();
-        ~Shell();
-        void run();
-        void login();
-        string GetPass();
-        int printPrompt();
+        void Login();
+        int GetCh();
+        string GetPassword();
+
+        int CShell();
+        int PrintPrompt();
+        int ParseCmd();
+
         void push_list(const string v1);
         void pop_list();
-        int cshell();
+
         void usage();
         int cat(const string v1);
         int ls();
         int send(const string v1);
         void printMember();
+
+        string GetPass();
+
         string banner = "\n\n::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n                     _ _____ _____    ___        _             \n                    | |  ___|  ___|  / _ \\ _ __ (_) ___  _ __  \n                 _  | | |_  | |_    | | | | '_ \\| |/ _ \\| '_ \\ \n                | |_| |  _| |  _|   | |_| | | | | | (_) | | | |\n                 \\___/|_|   |_|      \\___/|_| |_|_|\\___/|_| |_|\n                                                               \n                __  __                                           \n               |  \\/  | ___  ___ ___  __ _ _ __   __ _  ___ _ __ \n               | |\\/| |/ _ \\/ __/ __|/ _` | '_ \\ / _` |/ _ \\ '__|\n               | |  | |  __/\\__ \\__ \\ (_| | | | | (_| |  __/ |   \n               |_|  |_|\\___||___/___/\\__,_|_| |_|\\__, |\\___|_|   \n                                                 |___/           \n::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n";
 };
 
